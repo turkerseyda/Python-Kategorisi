fc00cfce0cedc644f70b4aa2dd913e1b6880e9ce
şeyda türker <sydskr_1991@hotmail.com>
2024-02-06 00:33:59 +0300

    eklenti yapildi

---
+++ 005-Degisken-Tanimlama-ve-Degisken-Tanimlama-Kurallari.py
@@ -0,0 +1,34 @@
+# https://winnercrespo.com/naming-conventions/
+
+# Degisken isimleri nasil olmalidir ?
+# g14 = "Python"  # Bu degisken adi anlasilir mi? veya akilda tutulabilir mi?
+# print(g14)
+x = 1
+# x, i, y, z gibi degisken isimleri kullanmayin
+
+# Degisken isimleri ingilizce mi olmali ?
+# kullanıcı_adı = "Gamze"
+first_name = "Gamze" # Lütfen degisken isimleri ingilizce kullanin
+
+# Degisken isimleri rakamla baslayabilir mi ?
+# 123_deneme = "Deneme"  --> rakamla baslayamaz
+d_1234 = "Deneme"  # bu kullanilabilir
+
+# Degisken isimleri büyük harfle yazilabilir mi ?
+password = "1234" # BUYUK HARFLE OLMALI MIYDI?
+SERVER_IP = "127.0.0.1" # büyük harle yazilmasi yani bu bilgi degismeyecek bi bilgi
+
+# Degisken isimleri birden fazla kullanilabilir mi ?
+print(first_name)
+first_name = "Aylin"
+print(first_name)
+
+# Degisken isimleri büyük harf yazmakla kücük harfle yazmak arasinda fark var mi ?
+# Eger bir bilgi büyük harfle yaziliyorsa bu bilgi degismeyecek bilgidir..
+
+# Degisken isimleri büyük harfle(USER_NAME) ve kücük harfle(user_name) yazilsada ayni sonucu alir miyiz ?
+user_name = "Lorem ipsum"
+user_Name = "Lorem ipsum dolor sömkmckam"
+USER_NAME = "Lorem 2"
+
+print(user_name)
---
+++ 006-Python-i-Anlamak-(Zen-of-Python).py
@@ -0,0 +1,5 @@
+# https://medium.com/@hakanyalcinkaya/python-ve-kod-yazma-sanat%C4%B1-855eb6382dcb
+
+# >>> import this
+
+# python -m this
